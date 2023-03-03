6c40ef8656a636a361102c270e28e14e1aa979ec
Andrew Berisha-Cornejo <andrew@Andrews-MacBook-Pro.local>
2023-03-02 23:25:43 -0600

    add a fruit list table with nutrition facts
    Signed-off-by: Andrew Berisha-Cornejo <andrew@Andrews-MacBook-Pro.local>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # This is a new file for the Streamlit App
 
 import streamlit
+import pandas
 
 streamlit.title("My Mom's New Healthy Diner")
 
@@ -11,3 +12,8 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+fruit_list_url = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt'
+my_fruit_list = pandas.read_csv(fruit_list_url)
+streamlit.dataframe(my_fruit_list)
+
