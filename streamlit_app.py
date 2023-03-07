7d7b3420190c7a66c6a69df3f6155390c4cb99a0
Andrew Berisha-Cornejo <andrew@Andrews-MacBook-Pro.local>
2023-03-07 14:11:53 -0600

    add fruityvice section
    Signed-off-by: Andrew Berisha-Cornejo <andrew@Andrews-MacBook-Pro.local>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 # This is a new file for the Streamlit App
-
+import requests
 import streamlit
 import pandas
 
@@ -25,3 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+
+# New section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
